c3316538da3797383c063bf7a285bd1924af2ed9
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-11-10 11:06:59 +0100

    Patience

--- Challenge 1/code/model_efficientNet_M.py
+++ Challenge 1/code/model_efficientNet_M.py
@@ -284,7 +284,7 @@ def train_model():
         batch_size = 32,
         epochs = 1000,
         validation_data = (images_val, labels_val), # We need to apply the preprocessing thought for the MobileNetV2 network
-        callbacks = [tfk.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=20, restore_best_weights=True)]
+        callbacks = [tfk.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=100, restore_best_weights=True)]
     ).history
 
     # Save the best model
@@ -320,7 +320,7 @@ def train_model():
         batch_size = 32,
         epochs = 1000,
         validation_data = (images_val, labels_val), # We need to apply the preprocessing thought for the MobileNetV2 network
-        callbacks = [tfk.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=20, restore_best_weights=True)]
+        callbacks = [tfk.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=100, restore_best_weights=True)]
     ).history
 
     # Save the model
